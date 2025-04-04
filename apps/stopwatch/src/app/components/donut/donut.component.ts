7303d89df7d4855214da4a9febe27c10de5df7e4
Nicola Bello <nicola.bello@tide.co>
2025-04-04 14:21:05 +0100

    transformed to countdown

--- apps/stopwatch/src/app/components/donut/donut.component.ts
+++ apps/stopwatch/src/app/components/donut/donut.component.ts
@@ -16,20 +16,28 @@ export class DonutComponent {
   strokeDasharray = computed(() => {
     const max = 100;
     const elapsedPercent = Math.min(max, Math.round((this.elapsed() / this.total()) * max * 100) / 100);
-    return `${elapsedPercent} ${max - elapsedPercent}`;
+    return `${max - elapsedPercent} ${elapsedPercent}`;
   });
 
-  time = computed(() => {
-    const hours = Math.floor(this.elapsed() / 3600);
-    const minutes = Math.floor((this.elapsed() % 3600) / 60);
-    const seconds = this.elapsed() % 60;
+  elapsedTotal = computed(() => this.formatSeconds(this.elapsed()));
 
-    const hoursStr = hours ? `${hours}:` : '';
-    const minutesStr = `${minutes}`.padStart(2, '0');
-    const secondsStr = `${seconds}`.padStart(2, '0');
+  elapsedDifference = computed(() => this.formatSeconds(this.elapsed() - this.total(), true));
 
-    return `${hoursStr ? `${hoursStr}:` : ''}${minutesStr}:${secondsStr}`;
-  });
+  elapsedRemainder = computed(() => this.formatSeconds(Math.max(0, this.total() - this.elapsed())));
+
+  timeUp = computed(() => this.total() - this.elapsed() < 0);
+
+  formatSeconds(s: number, addSign = false) {
+    const sign = s > 0 ? '+' : s < 0 ? '-' : '';
+
+    s = Math.abs(s);
+
+    const hours = `${Math.floor(s / 3600)}`.padStart(2, '0');
+    const minutes = `${Math.floor((s % 3600) / 60)}`.padStart(2, '0');
+    const seconds = `${s % 60}`.padStart(2, '0');
+
+    return `${addSign ? sign : ''}${hours !== '00' ? `${hours}:` : ''}${minutes}:${seconds}`;
+  }
 
   #elapsedTransform(value: number): number {
     return Math.min(60 * 59 + 59, Math.max(0, value));
